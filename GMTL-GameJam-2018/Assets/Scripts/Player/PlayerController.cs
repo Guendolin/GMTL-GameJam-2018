8c534bb61f5817c36573702aaad136eef3d36c5e
Guendolin <floggingluna@gmail.com>
2018-09-01 12:31:45 +0200

    Added temp controlls and temp materials.

---
+++ GMTL-GameJam-2018/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera mainCamera;
+
+    [Space]
+
+    public Transform bat;
+
+    [Space]
+
+    public PlayerInput playerInput;
+
+    private const float MOVEMENT_SPEED = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+
+        if(mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+
+
+        playerInput.Init(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 movement = playerInput.GetMovement();
+
+        bool aimPressent;
+        Vector2 aim = playerInput.GetAim(out aimPressent);
+        Vector3 movementTranslate = (MOVEMENT_SPEED * Time.deltaTime) * movement;
+
+        Vector3 newPos = transform.position + movementTranslate;
+        transform.position = newPos;
+
+        if (aimPressent)
+        {
+            float angle = Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg;
+            Quaternion batRotation = Quaternion.Euler(0, 0, angle);
+
+            bat.localRotation = batRotation;
+        }
+
+        Debug.DrawRay(transform.position, movement, Color.red);
+        if (aimPressent)
+        {
+            Debug.DrawRay(transform.position, aim, Color.green);
+        }
+
+        
+    }
+}
---
+++ GMTL-GameJam-2018/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class InputNames
+{
+    public const string VERTICAL = "Vertical";
+    public const string HORIZONTAL = "Horizontal";
+}
+
+
+[System.Serializable]
+public class PlayerInput {
+    private PlayerController _playerController;
+
+    public void Init(PlayerController playerController)
+    {
+        _playerController = playerController;
+    }
+
+    public Vector2 GetMovement()
+    {
+        return new Vector2(Input.GetAxisRaw(InputNames.HORIZONTAL), Input.GetAxisRaw(InputNames.VERTICAL));
+    }
+
+    public Vector2 GetAim(out bool aimPressent)
+    {
+        Vector2 mouseScreenPos = Input.mousePosition;
+        Vector2 playerScreenPos = _playerController.mainCamera.WorldToScreenPoint(_playerController.transform.position);
+
+        aimPressent = true;
+
+        return (mouseScreenPos - playerScreenPos).normalized;
+    }
+
+}
