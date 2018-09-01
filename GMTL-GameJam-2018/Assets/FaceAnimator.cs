21e8bfd8266934c78d0c27812f44b5fbb81e384f
topatisen <johannes.fornaeus@gmail.com>
2018-09-01 13:45:20 +0200

    Made player sprite animator
    Player sprite animation

---
+++ GMTL-GameJam-2018/Assets/FaceAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceAnimator : MonoBehaviour {
+
+	private Animator anim;
+
+	
+	public float blinkInterval;
+
+	public float randomInterval;
+
+	private float randomExtra;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		randomExtra = Random.Range(-randomInterval, randomInterval);
+
+		StartCoroutine(BlinkRoutine());
+	}
+
+	IEnumerator BlinkRoutine()
+	{
+		yield return new WaitForSeconds(blinkInterval+randomExtra);
+
+		anim.SetTrigger("Blink");
+
+		StartCoroutine(BlinkRoutine());
+	}
+
+}
